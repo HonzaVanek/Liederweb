e8c1342340e31e1a7a8612ff61695960ee1d4197
Jan Vaněk <vanek.hv@gmail.com>
2026-06-06 23:14:35 +0200

    kurzíva na veřejné stránce koncertu

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,5 +1,12 @@
 from django import template
 
+
+import re
+
+from django.template.defaultfilters import linebreaks
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
 from core.models import Partner
 
 register = template.Library()
@@ -20,3 +27,35 @@ def render_partners_section(context):
         "request": request,
         "partners": partners,
     }
+
+ITALIC_RE = re.compile(r"\*(?!\s)(.+?)(?<!\s)\*")
+
+
+@register.filter(needs_autoescape=True)
+def event_richtext(value, autoescape=True):
+    """
+    Bezpečný jednoduchý formátovač pro veřejné texty koncertu.
+
+    Povolí:
+    - odstavce a odřádkování
+    - kurzívu přes <em>...</em>
+    - kurzívu přes *...*
+
+    Ostatní HTML zůstane escapované.
+    """
+    if not value:
+        return ""
+
+    text = str(conditional_escape(value)) if autoescape else str(value)
+
+    text = (
+        text
+        .replace("&lt;em&gt;", "<em>")
+        .replace("&lt;/em&gt;", "</em>")
+        .replace("&lt;i&gt;", "<em>")
+        .replace("&lt;/i&gt;", "</em>")
+    )
+
+    text = ITALIC_RE.sub(r"<em>\1</em>", text)
+
+    return mark_safe(linebreaks(text, autoescape=False))
